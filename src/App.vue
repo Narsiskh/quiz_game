7cb43557bff3b93789d3f3e3839a602227c2d219
Narsis <narsiskh.2013@gmail.com>
2023-11-04 20:13:20 +0330

    Creating Quiz Game.

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import q from "./data/quizes.json"
+import {ref} from "vue";
+
+const quizes = ref(q)
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input type="text">
+    </header>
+    <div class="options-container">
+      <div v-for="quiz in quizes" :key="quiz.id" class="card">
+        <img src="/math.jpg" alt="">
+        <div class="card-text">
+          <h2>Math</h2>
+          <p>15 Questions</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style src="./assets/style.sass" scoped lang="sass"></style>
