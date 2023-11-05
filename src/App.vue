4335178dae8624a297eab55c7c3b20a41b4b44d6
Narsis <narsiskh.2013@gmail.com>
2023-11-05 20:41:17 +0330

    Creating Quiz Game.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <script setup>
 import q from "./data/quizes.json"
-import {ref} from "vue";
+import {ref, watch} from "vue";
 
 const quizes = ref(q)
+const search = ref("")
+
+watch(search,() => {
+  quizes.value = q.filte
+  r(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
+})
 </script>
 
 <template>
   <div class="container">
     <header>
       <h1>Quizes</h1>
-      <input type="text">
+      <input v-model.trim ="search" type="text" placeholder="Search...">
     </header>
     <div class="options-container">
       <div v-for="quiz in quizes" :key="quiz.id" class="card">
-        <img src="/math.jpg" alt="">
+        <img :src="quiz.img" alt="">
         <div class="card-text">
-          <h2>Math</h2>
-          <p>15 Questions</p>
+          <h2>{{ quiz.name }}</h2>
+          <p>{{ quiz['questions'].length }}Questions</p>
         </div>
       </div>
     </div>
